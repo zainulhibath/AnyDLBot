b70b45ad97896e851ba578be0105700d7dd8a751
Zainul Abid <48980248+zainulhibath@users.noreply.github.com>
2019-12-16 03:51:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ class Config(object):
     TG_MAX_FILE_SIZE = 1572864000
     FREE_USER_MAX_FILE_SIZE = 50000000
     # chunk size that should be used with requests
-    CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
+    CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 256))
     # default thumbnail to be used in the videos
     DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://placehold.it/90x90")
     # for storing the user details
